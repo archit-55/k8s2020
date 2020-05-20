51322a8bc358f351277e3b0af5f561757081db8c
archit-55 <architagrawal55@gmail.com>
2020-05-20 18:18:13 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+~#!/bin/bash
+if [ $x = app1 ]
+then
+git clone https://github.com/mdn/beginner-html-site-styled.git
+httpd -DFOREGROUND
+else
+git clone https://github.com/microsoft/project-html-website.git
+httpd -DFOREGROUND
+fi
+~                                                                                                                                             
+~                                                                                                                                             
+~                                                                                                                                             
+~                            
